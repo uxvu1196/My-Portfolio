2423e21807d7ab01fccbca6e542ab3196f7d138c
uxvu1196 <uxvu1196@gmail.com>
2021-12-15 15:26:50 +0100

    netlify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
 import "../assets/scss/home.scss"
+import clouds from "../assets/img/clouds.jpg"
 
 
 export const Home = () => {
@@ -11,10 +12,7 @@ export const Home = () => {
             </div>
             <h1 className="gradient">Hey, welcome to my Portfolio.</h1>
 
-            <div>
-                <img src="/img/cloud.jpg" alt="logo" />
-            </div>
-
+            <img src={clouds} alt="" />
         </div>
     )
 }
